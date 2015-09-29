469fd912e3f5d79b0d4da723f9a5c0d787a78759
donnitazen <donna@zeniuseducation.com>
2015-09-29 18:02:43 +0700

    last 2 weeks

--- src/clj/pissaz/core.clj
+++ src/clj/pissaz/core.clj
@@ -30,3 +30,25 @@
   (stop)
   (start))
 
+(defn translate-roman-numeral
+  [roman-numeral]
+  (let [base {\I 1 \V 5 \X 10 \L 50 \C 100 \D 500 \M 1000}
+        roman-number (map base roman-numeral)
+        maxnum (cond
+                 (empty? roman-numeral) 0
+                 (= 1 (count roman-numeral)) (first roman-number)
+                 :else (apply max roman-number))
+        left-side (take-while #(< (base %) maxnum) roman-numeral)
+        right-side (rest (drop-while #(< (base %) maxnum) roman-numeral))]
+    (cond
+      (empty? roman-numeral) 0
+      :else (+ (- maxnum (translate-roman-numeral left-side)) (translate-roman-numeral right-side)))
+    ))
+
+(defn to-roman-number
+  [num]
+  (let [jumlah (count (str num))
+        pisah (map * (map #(- (int %) 48) (str num))
+                      (iterate #(/ % 10) (apply * (repeat jumlah 10))))]
+    pisah))
+
